faa5c4c16a94457c4e064dabe59e21b289e9b499
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-23 06:07:24 +0200

    Update Sphere.cpp

--- 2023/Group C/B. Sphere/Solution/Sphere.cpp
+++ 2023/Group C/B. Sphere/Solution/Sphere.cpp
@@ -1,6 +1,16 @@
-// Solution for Sphere
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
+    double radius, volume = 0, surfaceArea = 0;
+    cin >> radius;
+
+    volume = (4 / 3.0) * M_PI * (radius * radius * radius);
+    surfaceArea = 4 * M_PI * radius * radius;
+
+    cout << fixed << setprecision(3) << volume << " " << surfaceArea << endl;
     return 0;
 }
