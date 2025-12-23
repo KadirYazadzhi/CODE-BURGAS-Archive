03061b25c63c71a7d1ad5b4beed688e8719611e5
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-23 06:07:29 +0200

    Update T-Shirts.cpp

--- 2023/Group C/D. T-Shirts/Solution/T-Shirts.cpp
+++ 2023/Group C/D. T-Shirts/Solution/T-Shirts.cpp
@@ -1,6 +1,32 @@
-// Solution for T-Shirts
 #include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+pair<int, int> findMinMaxNumber(int numbers[], int n) {
+    int min = INT_MAX;
+    int max = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] < min) min = numbers[i];
+        if (numbers[i] > max) max = numbers[i];
+    }
+
+    return make_pair(min, max);
+}
 
 int main() {
+    int n;
+    cin >> n;
+
+    int numbers[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    pair<int, int> minMax = findMinMaxNumber(numbers, n);
+
+    cout << minMax.first << " " << minMax.second << endl;
+
     return 0;
 }
