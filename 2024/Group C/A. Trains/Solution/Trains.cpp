761299e56151bda679db60147a25f3d6bb2eaab1
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-27 00:08:29 +0200

    Update Trains.cpp

--- 2024/Group C/A. Trains/Solution/Trains.cpp
+++ 2024/Group C/A. Trains/Solution/Trains.cpp
@@ -1,6 +1,17 @@
-// Solution for Trains
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    int firstTrainSpeed, secondTrainSpeed, distance, time, totalTraveledDistance;
+    cin >> firstTrainSpeed;
+    cin >> secondTrainSpeed;
+    cin >> distance;
+
+    time = distance / (firstTrainSpeed + secondTrainSpeed);
+    totalTraveledDistance = (firstTrainSpeed * time) + (secondTrainSpeed * time);
+
+    cout << time << endl;
+    cout << totalTraveledDistance << endl;
     return 0;
 }
