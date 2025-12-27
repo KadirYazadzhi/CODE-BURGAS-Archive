965d05c92efa77a28a66b9a458de1a698bee16e9
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-27 23:45:34 +0200

    Update The Dancing Square.cpp

--- 2024/Group C/C. The Dancing Square/Solution/The Dancing Square.cpp
+++ 2024/Group C/C. The Dancing Square/Solution/The Dancing Square.cpp
@@ -1,6 +1,38 @@
-// Solution for The Dancing Square
 #include <iostream>
 
+using namespace std;
+
+struct Square {
+    int x;
+    int y;
+    int a;
+};
+
+Square square;
+
+void move(int dx, int dy) {
+    square.x += dx;
+    square.y += dy;
+}
+
+void print() {
+    cout << square.x << " " << square.y << " " << square.a << endl;
+}
+
 int main() {
+    int x, y, a;
+
+    cin >> x >> y >> a;
+
+    square.x = x;
+    square.y = y;
+    square.a = a;
+
+    int dx, dy;
+    cin >> dx >> dy;
+
+    move(dx, dy);
+    print();
+
     return 0;
 }
