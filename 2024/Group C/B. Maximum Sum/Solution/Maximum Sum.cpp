26f9a8dcc5caa0cbe7af5b875115c0e24011179c
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-27 23:45:31 +0200

    Update Maximum Sum.cpp

--- 2024/Group C/B. Maximum Sum/Solution/Maximum Sum.cpp
+++ 2024/Group C/B. Maximum Sum/Solution/Maximum Sum.cpp
@@ -1,6 +1,38 @@
-// Solution for Maximum Sum
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubarraySum(vector<int>& numbers) {
+    int n = numbers.size();
+
+    if (n == 0) return 0;
+
+    int previous = numbers[0];
+    int current = max(previous, 0);
+    int maxSum = previous;
+
+    for (int i = 1; i < n; i++) {
+        int temp = current;
+        current = max(current, previous + numbers[i]);
+        previous = temp;
+        maxSum = max(maxSum, current);
+    }
+
+    return maxSum;
+}
 
 int main() {
+    int n;
+    cin >> n;
+
+    vector<int> numbers(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    cout << maxSubarraySum(numbers) << endl;
+
     return 0;
 }
