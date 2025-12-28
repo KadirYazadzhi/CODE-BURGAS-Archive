73caff1ff9f6ecae0850108acdb4d48ef6133896
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-28 02:23:40 +0200

    Update Trains.cpp

--- 2025/Group C/B. Trains/Solution/Trains.cpp
+++ 2025/Group C/B. Trains/Solution/Trains.cpp
@@ -1,6 +1,27 @@
-// Solution for Trains
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    long long A, B, C, D, x;
+
+    if (!(cin >> A >> B >> C >> D >> x)) return 0;
+
+    long long numerator = A * x + B;
+    long long denominator = C * x + D;
+
+    if (denominator == 0) {
+        cout << "ZERO ERROR" << endl;
+        return 0;
+    }
+
+    if (numerator % denominator != 0) {
+        cout << "NAN ERROR" << endl;
+        return 0;
+    }
+
+    long long V = numerator / denominator;
+    cout << V << " km/h" << endl;
+
     return 0;
 }
