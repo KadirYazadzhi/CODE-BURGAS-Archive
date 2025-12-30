d90b82dbb7edcfa847cdccd4fa15ad2855481abd
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-30 03:54:02 +0200

    Update Binary Numbers.cpp

--- 2020/Group B/B. Binary Numbers/Solution/Binary Numbers.cpp
+++ 2020/Group B/B. Binary Numbers/Solution/Binary Numbers.cpp
@@ -1,6 +1,39 @@
-// Solution for Binary Numbers
 #include <iostream>
 
+using namespace std;
+
+int Bits(int n) {
+    int count = 0;
+
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+    }
+
+    return count;
+}
+
+int Diff(int x, int y) {
+    if (x > y) return x - y;
+    else if (y > x) return y - x;
+    else return 0;
+}
+
 int main() {
+    int n, m, next, number, diff = 1000;
+    cin >> n >> m;
+
+    int bits = Bits(m);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> next;
+
+        if (Diff(bits, diff) > Diff(bits, next)) {
+            number = i;
+            diff = next;
+        }
+    }
+
+    cout << number << endl;
     return 0;
 }
