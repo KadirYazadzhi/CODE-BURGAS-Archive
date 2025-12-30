73929e89fcdb2a8804921754904ba20b81dc895d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-30 03:54:00 +0200

    Update Mobile Deal.cpp

--- 2020/Group B/A. Mobile Deal/Solution/Mobile Deal.cpp
+++ 2020/Group B/A. Mobile Deal/Solution/Mobile Deal.cpp
@@ -1,6 +1,24 @@
-// Solution for Mobile Deal
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, n, sum = 0;
+        cin >> x >> n;
+
+        vector<int> megabytesPerMonth = vector<int>(n);
+
+        for (int i = 0; i < n; i++) cin >> megabytesPerMonth[i];
+
+        for (int i = 0; i < n; i++) sum += x + - megabytesPerMonth[i];
+        sum += x;
+
+        cout << sum << endl;
+    }
     return 0;
 }
