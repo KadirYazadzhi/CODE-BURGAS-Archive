1fd8b51191f7f0fa9818c35d430032e554141c60
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-30 03:53:49 +0200

    Update Digital Rivers 1.cpp

--- 2019/Group B/B. Digital Rivers 1/Solution/Digital Rivers 1.cpp
+++ 2019/Group B/B. Digital Rivers 1/Solution/Digital Rivers 1.cpp
@@ -1,6 +1,27 @@
-// Solution for Digital Rivers 1
 #include <iostream>
 
+using namespace std;
+
+long long Sum(long long num) {
+    if (num < 10) return num;
+    else return num % 10 + Sum(num / 10);
+}
+
 int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        long long r1, r2;
+        cin >> r1 >> r2;
+
+        while (r1 != r2) {
+            if (r1 < r2) r1 += Sum(r1);
+            else r2 += Sum(r2);
+        }
+
+        cout << r2 << endl;
+    }
+
     return 0;
 }
