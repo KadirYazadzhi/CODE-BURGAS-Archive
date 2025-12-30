1eda732fe33558925495e6770b6ae91e88d214fd
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-30 03:53:54 +0200

    Update Doner.cpp

--- 2019/Group B/D. Doner/Solution/Doner.cpp
+++ 2019/Group B/D. Doner/Solution/Doner.cpp
@@ -1,6 +1,35 @@
-// Solution for Doner
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        cin >> n;
+
+        vector<int> nums = vector<int>(n);
+        for (int j = 0; j < n; j++) cin >> nums[j];
+
+        cin >> m;
+
+        sort(nums.begin(), nums.end());
+
+        int minDiff = INT_MAX;
+
+        for (int j = 0; j <= n - m; j++) {
+            int currentDiff = nums[j + m - 1] - nums[j];
+
+            if (currentDiff < minDiff) minDiff = currentDiff;
+        }
+
+        cout << minDiff << endl;
+    }
+
     return 0;
 }
