d9d8ed698999198f6d088658ff7e7a1330cadb92
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-31 20:23:41 +0200

    Update Sudoku.cpp

--- 2021/Group B/A. Sudoku/Solution/Sudoku.cpp
+++ 2021/Group B/A. Sudoku/Solution/Sudoku.cpp
@@ -1,6 +1,64 @@
-// Solution for Sudoku
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isValid(vector<string>& board, int row, int col, char c) {
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == c) return false;
+        if (board[i][col] == c) return false;
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+    }
+
+    return true;
+}
+
+bool solve(vector<string>& board) {
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            if (board[r][c] != '.') continue;
+
+            for (char ch = 'A'; ch <= 'I'; ch++) {
+                if (isValid(board, r, c, ch)) {
+                    board[r][c] = ch;
+
+                    if (solve(board)) return true;
+
+                    board[r][c] = '.';
+                }
+            }
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void solveSudoku() {
+    string input;
+    cin >> input;
+
+    vector<string> board(9, string(9, ' '));
+    for (int i = 0; i < 81; i++) board[i / 9][i % 9] = input[i];
+
+    if (solve(board)) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                cout << board[r][c];
+            }
+        }
+
+        cout << endl;
+    }
+}
 
 int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) solveSudoku();
+
     return 0;
 }
