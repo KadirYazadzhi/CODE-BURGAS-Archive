6c3a231ee5b2e5a30adfb1ed73094b6adde29399
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-31 23:34:53 +0200

    Update Sudoku.cpp

--- 2021/Group B/A. Sudoku/Solution/Sudoku.cpp
+++ 2021/Group B/A. Sudoku/Solution/Sudoku.cpp
@@ -4,51 +4,81 @@
 
 using namespace std;
 
-bool isValid(vector<string>& board, int row, int col, char c) {
-    for (int i = 0; i < 9; i++) {
-        if (board[row][i] == c) return false;
-        if (board[i][col] == c) return false;
-        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
-    }
+int rows[9], cols[9], boxes[9];
 
-    return true;
+int charToBit(char ch) { return 1 << (ch - 'A'); }
+
+void toggle(int r, int c, char ch, bool set) {
+    int bit = charToBit(ch);
+
+    if (set) {
+        rows[r] |= bit;
+        cols[c] |= bit;
+        boxes[(r / 3) * 3 + c / 3] |= bit;
+    }
+    else {
+        rows[r] &= ~bit;
+        cols[c] &= ~bit;
+        boxes[(r / 3) * 3 + c / 3] &= ~bit;
+    }
 }
 
 bool solve(vector<string>& board) {
+    int minOpts = 10;
+    int bestR = -1, bestC = -1;
+
     for (int r = 0; r < 9; r++) {
         for (int c = 0; c < 9; c++) {
-            if (board[r][c] != '.') continue;
+            if (board[r][c] == '.') {
+                int allowed = ~(rows[r] | cols[c] | boxes[(r / 3) * 3 + c / 3]) & 0x1FF;
+                int count = 0;
 
-            for (char ch = 'A'; ch <= 'I'; ch++) {
-                if (isValid(board, r, c, ch)) {
-                    board[r][c] = ch;
+                for (int i = 0; i < 9; i++) if (allowed & (1 << i)) count++;
 
-                    if (solve(board)) return true;
-
-                    board[r][c] = '.';
+                if (count < minOpts) {
+                    minOpts = count;
+                    bestR = r;
+                    bestC = c;
                 }
             }
-
-            return false;
         }
     }
 
-    return true;
+    if (bestR == -1) return true;
+    if (minOpts == 0) return false;
+
+    int allowed = ~(rows[bestR] | cols[bestC] | boxes[(bestR / 3) * 3 + bestC / 3]) & 0x1FF;
+    for (int i = 0; i < 9; i++) {
+        if (allowed & (1 << i)) {
+            char ch = 'A' + i;
+            board[bestR][bestC] = ch;
+            toggle(bestR, bestC, ch, true);
+
+            if (solve(board)) return true;
+
+            toggle(bestR, bestC, ch, false);
+            board[bestR][bestC] = '.';
+        }
+    }
+
+    return false;
 }
 
 void solveSudoku() {
     string input;
     cin >> input;
 
+    for (int i = 0; i < 9; i++) rows[i] = cols[i] = boxes[i] = 0;
+
     vector<string> board(9, string(9, ' '));
-    for (int i = 0; i < 81; i++) board[i / 9][i % 9] = input[i];
+    for (int i = 0; i < 81; i++) {
+        board[i / 9][i % 9] = input[i];
+
+        if (input[i] != '.') toggle(i / 9, i % 9, input[i], true);
+    }
 
     if (solve(board)) {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                cout << board[r][c];
-            }
-        }
+        for (int r = 0; r < 9; r++) cout << board[r];
 
         cout << endl;
     }
