e9934dc0cf5d8a467a7c58fdbe27d733069767d2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-01 13:23:04 +0200

    Update Hills.cpp

--- 2023/Group B/B. Hills/Solution/Hills.cpp
+++ 2023/Group B/B. Hills/Solution/Hills.cpp
@@ -1,6 +1,32 @@
-// Solution for Hills
 #include <iostream>
+#include <climits>
+#include <vector>
+#include <sstream>
+
+using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long currentHill;
+
+    long long maxSoFar = LLONG_MIN;
+    long long currentMax = 0;
+
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+
+    while (ss >> currentHill) {
+        currentMax += currentHill;
+
+        if (maxSoFar < currentMax) maxSoFar = currentMax;
+
+        if (currentMax < 0) currentMax = 0;
+    }
+
+    cout << maxSoFar << endl;
+
     return 0;
 }
