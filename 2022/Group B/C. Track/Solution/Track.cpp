df769d6374adcb16ef457cd3707b2cfbe7374a61
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-01 03:07:41 +0200

    Update Track.cpp

--- 2022/Group B/C. Track/Solution/Track.cpp
+++ 2022/Group B/C. Track/Solution/Track.cpp
@@ -1,6 +1,35 @@
-// Solution for Track
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
+    long long X;
+    int N;
+
+    cin >> X >> N;
+
+    long long totalDistance1 = 0, totalDistance2 = 0;
+
+    for (int i = 0; i < N; i++) {
+        long long v1, t1, v2, t2;
+        cin >> v1 >> t1 >> v2 >> t2;
+
+        totalDistance1 += v1 * t1;
+        totalDistance2 += v2 * t2;
+    }
+
+    long long pos1 = totalDistance1 % X;
+    if (pos1 < 0) pos1 += X;
+
+    long long pos2 = totalDistance2 % X;
+    if (pos2 < 0) pos2 += X;
+
+    long long diff = abs(pos1 - pos2);
+    long long result = min(diff, X - diff);
+
+    cout << result << endl;
+
     return 0;
 }
