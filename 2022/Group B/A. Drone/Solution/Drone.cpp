049fb6dc9369e9d839fdd20ae6e6fb6f9c1c10b2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-01 03:07:36 +0200

    Update Drone.cpp

--- 2022/Group B/A. Drone/Solution/Drone.cpp
+++ 2022/Group B/A. Drone/Solution/Drone.cpp
@@ -1,6 +1,39 @@
-// Solution for Drone
 #include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
 
 int main() {
+    int l, n;
+    cin >> l >> n;
+
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++) cin >> numbers[i];
+
+    vector<int> visibleNumbers;
+    int currentMax = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] > currentMax) {
+            currentMax = numbers[i];
+
+            if (i + l < n) {
+                bool canHide = true;
+
+                for (int j = i + 1; j <= i + l; j++) {
+                    if (numbers[j] + 1 > numbers[i]) {
+                        canHide = false;
+                        break;
+                    }
+                }
+
+                if (canHide) visibleNumbers.push_back(i + 1);
+            }
+        }
+    }
+
+    if (visibleNumbers.empty()) cout << "NO" << endl;
+    else for (int num : visibleNumbers) cout << num << endl;
+
     return 0;
 }
