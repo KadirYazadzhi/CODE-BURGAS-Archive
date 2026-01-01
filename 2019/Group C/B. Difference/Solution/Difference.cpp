285b9cdea58bd1b692f9bef08c2c4db1fb8353aa
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-01 13:22:44 +0200

    Update Difference.cpp

--- 2019/Group C/B. Difference/Solution/Difference.cpp
+++ 2019/Group C/B. Difference/Solution/Difference.cpp
@@ -8,10 +8,11 @@ void solve() {
 
     int j = 1;
     for (int i = a + 1; i <= a + y; i++) {
-        if (i % j == 0) cout << i << " ";
+        if (i % j == 0) cout << i << (i == a + y ? "" : " ");
 
         j++;
     }
+    cout << endl;
 }
 
 int main() {
