cbda312f902b3052c1b2b1a09c635c029d28ed74
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-03 21:16:10 +0200

    Update The Secret Of Words.cpp

--- 2024/Group B/C. The Secret Of Words/Solution/The Secret Of Words.cpp
+++ 2024/Group B/C. The Secret Of Words/Solution/The Secret Of Words.cpp
@@ -1,6 +1,27 @@
-// Solution for The Secret Of Words
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string text;
+    cin >> text;
+
+    string pattern = "agent";
+    int count = 0;
+    size_t pos = text.find(pattern, 0);
+
+    while (pos != string::npos) {
+        count++;
+        pos = text.find(pattern, pos + pattern.length());
+    }
+
+    cout << count << endl;
+    if (count > 5) cout << "ALERT" << endl;
+    else cout << "NO ALERT" << endl;
+
     return 0;
 }
