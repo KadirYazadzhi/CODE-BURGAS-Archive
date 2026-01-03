f31e17c9a27076250d9f52aac74c5aee7db634a8
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-03 21:16:14 +0200

    Update Journey Through Time.cpp

--- 2024/Group B/D. Journey Through Time/Solution/Journey Through Time.cpp
+++ 2024/Group B/D. Journey Through Time/Solution/Journey Through Time.cpp
@@ -1,6 +1,51 @@
-// Solution for Journey Through Time
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool visited[1001];
+vector<int> adj[1001];
+vector<int> path;
+int N, M;
+
+bool dfs(int current, int target) {
+    visited[current] = true;
+    path.push_back(current);
+
+    if (current == target) return true;
+
+    for (int next : adj[current]) {
+        if (visited[next]) continue;
+
+        if (dfs(next, target)) return true;
+    }
+
+    path.pop_back();
+    return false;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    if (dfs(1, N)) {
+        cout << "TIME TRAVEL!" << endl;
+
+        for (int i = 0; i < path.size(); ++i)
+            cout << path[i] << (i == path.size() - 1 ? "" : " ");
+        cout << endl;
+    }
+    else cout << "TIME TRAVEL NOT POSSIBLE!" << endl;
+
     return 0;
 }
