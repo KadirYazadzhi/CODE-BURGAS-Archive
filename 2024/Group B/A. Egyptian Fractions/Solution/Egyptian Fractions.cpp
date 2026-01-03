2ab1fbe0800b39175099a2f9a1717ef3ee3ff790
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-03 21:16:04 +0200

    Update Egyptian Fractions.cpp

--- 2024/Group B/A. Egyptian Fractions/Solution/Egyptian Fractions.cpp
+++ 2024/Group B/A. Egyptian Fractions/Solution/Egyptian Fractions.cpp
@@ -1,6 +1,35 @@
-// Solution for Egyptian Fractions
 #include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+void printEgyptian(long long nr, long long dr) {
+    if (dr == 0 || nr == 0) return;
+    if (dr % nr == 0) {
+        cout << "1/" << dr / nr;
+        return;
+    }
+    if (nr > dr) {
+        cout << nr / dr << " + ";
+        printEgyptian(nr % dr, dr);
+        return;
+    }
+
+    long long x = dr / nr + 1;
+    cout << "1/" << x << " ";
+
+    printEgyptian(nr * x - dr, dr * x);
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long nr, dr;
+    cin >> nr >> dr;
+    printEgyptian(nr, dr);
+    cout << endl;
+
     return 0;
 }
