54349d263b383275f169e0b41fe2c7baa1de044a
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-04 22:43:33 +0200

    Update Sequence.cpp

--- 2022/Group A/C. Sequence/Solution/Sequence.cpp
+++ 2022/Group A/C. Sequence/Solution/Sequence.cpp
@@ -1,6 +1,39 @@
-// Solution for Sequence
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<long long> nums(n);
+    long long totalXor = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+        totalXor ^= nums[i];
+    }
+
+    long long rightMostSetBit = totalXor & -totalXor;
+
+    long long x = 0, y = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] & rightMostSetBit) x ^= nums[i];
+        else y ^= nums[i];
+    }
+
+    if (x > y) swap(x, y);
+
+    cout << x << " " << y << "\n";
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
     return 0;
 }
