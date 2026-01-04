3af8ccc2db5abd6df2692fb60ef816bf7ae05424
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-04 03:33:01 +0200

    Update Sequence.cpp

--- 2019/Group A/C. Sequence/Solution/Sequence.cpp
+++ 2019/Group A/C. Sequence/Solution/Sequence.cpp
@@ -1,6 +1,49 @@
-// Solution for Sequence
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Element {
+    int value;
+    int index;
+};
+
+bool compareByValue(const Element& a, const Element& b) {
+    return a.value < b.value;
+}
+
+bool compareByIndex(const Element& a, const Element& b) {
+    return a.index < b.index;
+}
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<Element> seq(N);
+    for (int i = 0; i < N; i++) {
+        cin >> seq[i].value;
+        seq[i].index = i;
+    }
+
+    sort(seq.begin(), seq.end(), compareByValue);
+
+    vector<Element> result;
+    for (int i = 0; i < M; i++) result.push_back(seq[i]);
+
+    sort(result.begin(), result.end(), compareByIndex);
+
+    for (int i = 0; i < M; i++)
+        cout << result[i].value << " ";
+    cout << endl;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
     return 0;
 }
