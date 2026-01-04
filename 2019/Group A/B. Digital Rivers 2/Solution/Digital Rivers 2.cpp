c1705c36af17d032ed6121042e68e9b1897f7b15
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-04 03:32:58 +0200

    Update Digital Rivers 2.cpp

--- 2019/Group A/B. Digital Rivers 2/Solution/Digital Rivers 2.cpp
+++ 2019/Group A/B. Digital Rivers 2/Solution/Digital Rivers 2.cpp
@@ -1,6 +1,45 @@
-// Solution for Digital Rivers 2
 #include <iostream>
 
+using namespace std;
+
+int sumDigits(int n) {
+    int sum = 0;
+
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
+
+void solve() {
+    int R1;
+    cin >> R1;
+
+    bool found = false;
+
+    int start = (R1 - 45 > 0) ? (R1 - 45) : 1;
+
+    for (int y = start; y < R1; y++) {
+        if (y + sumDigits(y) == R1) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) solve();
+
     return 0;
 }
