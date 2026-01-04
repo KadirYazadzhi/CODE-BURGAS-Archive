1ee767212c4eee459b1469442aa23b8cfcf02d6d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-04 22:43:29 +0200

    Update Chemistry.cpp

--- 2022/Group A/B. Chemistry/Solution/Chemistry.cpp
+++ 2022/Group A/B. Chemistry/Solution/Chemistry.cpp
@@ -1,6 +1,76 @@
-// Solution for Chemistry
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cctype>
+
+using namespace std;
+
+map<string, int> parseSide(string side) {
+    map<string, int> atomCounts;
+    size_t pos = 0;
+
+    while (pos < side.length()) {
+        size_t nextPlus = side.find('+', pos);
+        string molecule = side.substr(pos, nextPlus - pos);
+
+        int coefficient = 0;
+        size_t i = 0;
+        while (i < molecule.length() && isdigit(molecule[i])) {
+            coefficient = coefficient * 10 + (molecule[i] - '0');
+            i++;
+        }
+
+        if (coefficient == 0) coefficient = 1;
+
+        while (i < molecule.length()) {
+            if (isupper(molecule[i])) {
+                string element = "";
+                element += molecule[i];
+                i++;
+
+                int count = 0;
+                while (i < molecule.length() && isdigit(molecule[i])) {
+                    count = count * 10 + (molecule[i] - '0');
+                    i++;
+                }
+
+                if (count == 0) count = 1;
+                atomCounts[element] +=(coefficient * count);
+            }
+            else i++;
+        }
+
+        if (nextPlus == string::npos) break;
+        pos = nextPlus + 1;
+    }
+
+    return atomCounts;
+}
+
+void solve() {
+    string equation;
+    cin >> equation;
+
+    size_t arrowPos = equation.find("->");
+    string leftSide = equation.substr(0, arrowPos);
+    string rightSide = equation.substr(arrowPos + 2);
+
+    map<string, int> leftAtoms = parseSide(leftSide);
+    map<string, int> rightAtoms = parseSide(rightSide);
+
+    if (leftAtoms == rightAtoms) cout << "YES" << "\n";
+    else cout << "NO" << "\n";
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    while (n--) solve();
+
     return 0;
 }
