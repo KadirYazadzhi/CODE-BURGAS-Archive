9d3e2740cdc563e655735201a31add9d39a536eb
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-04 03:33:08 +0200

    Update Chess Rook.cpp

--- 2020/Group A/A. Chess Rook/Solution/Chess Rook.cpp
+++ 2020/Group A/A. Chess Rook/Solution/Chess Rook.cpp
@@ -1,6 +1,50 @@
-// Solution for Chess Rook
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int calculateDistance(string c1, string c2) {
+    int col1 = c1[0] - 'a';
+    int row1 = c1[1] - '1';
+    int col2 = c2[0] - 'a';
+    int row2 = c2[1] - '1';
+
+    return abs(col1 - col2) + abs(row1 - row2);
+}
+
+void solve() {
+    string line;
+    getline(cin, line);
+
+    stringstream ss(line);
+    string prevCord, currentCoord;
+
+    int totalDistance = 0;
+
+    if (ss >> prevCord) {
+        while (ss >> currentCoord) {
+            totalDistance += calculateDistance(prevCord, currentCoord);
+            prevCord = currentCoord;
+        }
+    }
+
+    cout << totalDistance << endl;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    string nStr;
+    if (getline(cin, nStr)) {
+        n = stoi(nStr);
+
+        while (n--) solve();
+    }
+
     return 0;
 }
