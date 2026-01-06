65038060ef386792ba215c2f0377c5d75ca18dcd
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-06 21:22:16 +0200

    Update Candies.cpp

--- 2024/Group A/A. Candies/Solution/Candies.cpp
+++ 2024/Group A/A. Candies/Solution/Candies.cpp
@@ -1,6 +1,49 @@
-// Solution for Candies
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Candy {
+    int weight;
+    int value;
+};
+
+bool compareCandies(const Candy& a, const Candy& b) {
+    if (a.weight != b.weight) {
+        return a.weight > b.weight;
+    }
+
+    return a.value > b.value;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long budget;
+    int n;
+
+    cin >> budget >> n;
+
+    vector<Candy> candies(n);
+    for (int i = 0; i < n; i++) {
+        cin >> candies[i].weight >> candies[i].value;
+    }
+
+    sort(candies.begin(), candies.end(), compareCandies);
+
+    long long totalValue = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (budget >= candies[i].weight) {
+            budget -= candies[i].weight;
+            totalValue += candies[i].value;
+        }
+        else break;
+    }
+
+    cout << totalValue << " " << budget << "\n";
+
     return 0;
 }
