3fcdacff6dd0099b22b1abeb4ab9414ff4a475c0
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-06 21:22:23 +0200

    Update Backpack.cpp

--- 2024/Group A/D. Backpack/Solution/Backpack.cpp
+++ 2024/Group A/D. Backpack/Solution/Backpack.cpp
@@ -1,6 +1,33 @@
-// Solution for Backpack
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int W, N;
+    cin >> W >> N;
+
+    vector<long long> dp(W + 1, 0);
+
+    for (int i = 0; i < N; ++i) {
+        int weight;
+        long long value;
+        cin >> weight >> value;
+
+        for (int j = W; j >= weight; --j) {
+            dp[j] = max(dp[j], dp[j - weight] + value);
+        }
+    }
+
+    cout << dp[W] << endl;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
     return 0;
 }
